529aeaa5159459d75d6a7d0018c8832b0a9de2ee
Degenddy <degenddy@users.noreply.github.com>
2021-12-27 17:53:42 +0200

    wowswap integration

---
+++ wowswap/apy.js
@@ -0,0 +1,80 @@
+const fetch = require("node-fetch");
+
+/**
+ *
+ */
+class APY {
+
+    static displayName = "APY";
+    static description = "Track your position's APY";
+
+    /**
+     * runs when class is initialized
+     *
+     * @param args
+     * @returns {Promise<void>}
+     */
+    async onInit(args) {
+
+        this.apiEndpoint = 'https://notifications.wowswap.io/notifications';
+
+    }
+
+    /**
+     * runs right before user subscribes to new notifications and populates subscription form
+     *
+     * @param args
+     * @returns {Promise<[{values: *[], id: string, label: string, type: string}, {id: string, label: string, type: string, value: string}, {default: number, description: string, id: string, label: string, type: string}]>}
+     */
+    async onSubscribeForm(args) {
+
+        return [];
+
+    }
+
+    /**
+     * runs when new blocks are added to the mainnet chain - notification scanning happens here
+     *
+     *  wowswap api return object:
+     *  [
+     *    {
+     *      "id": "string",
+     *       "message": "string",
+     *       "createdAt": 0,
+     *       "type": "apy",
+     *       "symbol": "string",
+     *       "value": 0,
+     *       "read": true
+     *     }
+     *  ]
+     *
+     * @param args
+     * @returns {Promise<{notification: string}|*[]>}
+     */
+    async onBlocks(args) {
+
+        const response = await fetch(this.apiEndpoint + '?address=' + args.address);
+        const wowswapApyNotifications = await response.json();
+
+        const notifications = [];
+
+        if (wowswapApyNotifications && wowswapApyNotifications.length > 0) {
+
+            for (const wowswapApyNotification of wowswapApyNotifications) {
+
+                notifications.push({
+                    uniqueId: wowswapApyNotification.createdAt,
+                    notification: wowswapApyNotification.message
+                });
+
+            }
+
+        }
+
+        return notifications;
+
+    }
+
+}
+
+module.exports = APY;
