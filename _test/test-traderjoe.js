e133097870ff934e0fbe32ec36fbb1e4bdd90664
Degenddy <degenddy@users.noreply.github.com>
2022-01-13 13:12:58 +0200

    minor fix to trader joe

--- _test/test-traderjoe.js
+++ _test/test-traderjoe.js
@@ -15,18 +15,17 @@ async function testTraderJoeLending(args) {
 
     // simulate subscribe form event
     const form = await accountHealth.onSubscribeForm(args);
-    form.actualThreshold = args.threshold? args.threshold: form[0].default;
-    console.log("Form: \n",  form, "\n");
+
+    console.log("Form: \n", form, "\n");
 
     // simulate user filling in the subscription form in the app
     args.subscription = {
-        threshold: form.actualThreshold
+        'borrow-limit': args['borrow-limit'] ? args['borrow-limit'] : form[0].default
     }
 
     // simulate on blocks event
     return accountHealth.onBlocks(args);
 }
-
 
 
 async function main() {
@@ -36,7 +35,7 @@ async function main() {
     // borrow limit above threshold (notify)
     console.log("Notification: ", await testTraderJoeLending(
         {
-            address:'0x0181c4c3514210d8f33fec7934a888b9574fbb74',//0x343ba440db35997170cc6402692a0803c360c48c',//0xd253c7cc525345922cb1de7824a753d21083dede',
+            address: '0x0181c4c3514210d8f33fec7934a888b9574fbb74',//0x343ba440db35997170cc6402692a0803c360c48c',//0xd253c7cc525345922cb1de7824a753d21083dede',
             threshold: 70,
         }
     ));
@@ -44,7 +43,7 @@ async function main() {
     // borrow limit below (no notification)
     console.log("Notification: ", await testTraderJoeLending(
         {
-            address:'0x0181c4c3514210d8f33fec7934a888b9574fbb74',
+            address: '0x0181c4c3514210d8f33fec7934a888b9574fbb74',
             threshold: 80,
         }
     ));
@@ -52,14 +51,14 @@ async function main() {
     // extreme borrow limit - caution liquidation risk - description
     console.log("Notification: ", await testTraderJoeLending(
         {
-            address:'0x0153767a621aae02061380ec465ff91d8d517094',
+            address: '0x0153767a621aae02061380ec465ff91d8d517094',
         }
     ));
 
     // extreme borrow limit + no notification
     console.log("Notification: ", await testTraderJoeLending(
         {
-            address:'0x0153767a621aae02061380ec465ff91d8d517094',
+            address: '0x0153767a621aae02061380ec465ff91d8d517094',
             threshold: 99.9
         }
     ));
@@ -67,7 +66,7 @@ async function main() {
     // collateral with no loan
     console.log("Notification: ", await testTraderJoeLending(
         {
-            address:'0xd253c7cc525345922cb1de7824a753d21083dede',
+            address: '0xd253c7cc525345922cb1de7824a753d21083dede',
         }
     ));
 }
