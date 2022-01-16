b7ab1f26f92ff49e40abd2500bbede2d59f17964
Deddy Dayag <daviddayag@gmail.com>
2022-01-16 13:01:45 +0200

    pancake-swap integration

---
+++ _test/test-pancakeswap.js
@@ -0,0 +1,54 @@
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider(require('./dev-keys.json').web3bsc));
+
+async function testTokensWorth(address, tokenAddress, threshold, above) {
+    const PositionWorth = require('../pancakeswap/tokens-worth');
+    const positionWorth = new PositionWorth();
+
+    // simulate init event
+    await positionWorth.onInit({
+        web3
+    });
+
+    // simulate subscribe form event
+    const form = await positionWorth.onSubscribeForm({
+        web3,
+        address: address
+    });
+
+    console.log(form);
+
+    // simulate user filling in the subscription form in the app
+    const subscription = {
+        'above-below': above ? '0' : '1',
+        tokenAddress: tokenAddress,
+        threshold: threshold
+    };
+
+    // simulate on blocks event
+    return positionWorth.onBlocks({
+        web3,
+        address: address,
+        subscription
+    });
+}
+
+async function main() {
+
+    console.log('Running manual test:');
+
+    const address = '0x3dacC571356e7D5dFB3b475d6922442Ec06B9005';
+    const tokenAddress = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82';
+
+    console.log(await testTokensWorth(
+        address,
+        tokenAddress,
+        "20500",
+        false
+    ));
+
+}
+
+main();
+
+
---
+++ pancakeswap/tokens-worth.js
@@ -0,0 +1,164 @@
+const ABIs = require('./abis.json');
+const BN = require("bignumber.js");
+
+const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
+const WBNB_TOKEN_ADDRESS = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
+const USDC_TOKEN_ADDRESS = '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d';
+
+const amountFormatter = Intl.NumberFormat('en', {notation: 'compact'});
+
+class TokensWorth {
+
+    static displayName = "Position Worth";
+    static description = "Track your position's total worth to protect against loss";
+
+    /**
+     * runs when class is initialized
+     *
+     * @param args
+     * @returns {Promise<void>}
+     */
+    async onInit(args) {
+
+        const usdcContract = new args.web3.eth.Contract(ABIs.erc20, USDC_TOKEN_ADDRESS);
+
+        this.usdcDecimals = await usdcContract.methods.decimals().call();
+
+        this.routerContract = new args.web3.eth.Contract(
+            ABIs.router,
+            ROUTER_ADDRESS
+        );
+
+    }
+
+
+    /**
+     * runs right before user subscribes to new notifications and populates subscription form
+     *
+     * @param args
+     * @returns {Promise<[{values: *[], id: string, label: string, type: string}, {default: number, description: string, id: string, label: string, type: string}]>}
+     */
+    async onSubscribeForm(args) {
+
+        return [
+            {
+                type: "input-text",
+                id: "tokenAddress",
+                label: "Token Address",
+                default: '',
+                description: "The Token contract address"
+            },
+            {
+                type: "input-select",
+                id: "above-below",
+                label: "Above/Below",
+                values: [
+                    {value: "0", label: "Above"},
+                    {value: "1", label: "Below"}
+                ]
+            },
+            {
+                type: "input-number",
+                id: "threshold",
+                label: "Threshold price",
+                default: 0,
+                description: "Notify me when the price of my tokens goes above/below this value in USD"
+            }
+        ];
+
+    }
+
+    // runs when new blocks are added to the mainnet chain - notification scanning happens here
+    async onBlocks(args) {
+
+        const web3 = args.web3;
+        const subscription = args.subscription;
+
+        const threshold = subscription['threshold'];
+        const tokenAddress = subscription['tokenAddress'];
+        const above = subscription["above-below"] === "0";
+
+        const tokenContract = new web3.eth.Contract(ABIs.lp, tokenAddress);
+
+        const walletTokensWorthInUsdBN = await this._getPositionWorthInUsdBN(
+            web3,
+            args.address,
+            tokenAddress,
+            tokenContract
+        );
+
+        const uniqueId = tokenAddress + "-" + above + "-" + threshold;
+
+        if ((above && walletTokensWorthInUsdBN.gt(threshold)) || (!above && walletTokensWorthInUsdBN.lt(threshold))) {
+
+            const tokenSymbol = await tokenContract.methods.symbol().call();
+
+            return {
+                uniqueId: uniqueId,
+                notification: `Your holdings in ${tokenSymbol} (${amountFormatter.format(walletTokensWorthInUsdBN)}) ${above ? 'are above' : 'has dropped below'} ${amountFormatter.format(threshold)} USD`
+            };
+
+        } else {
+
+            return [];
+
+        }
+
+    }
+
+    /**
+     *
+     * @param walletAddress
+     * @param tokenContract
+     * @returns {Promise<BigNumber>}
+     * @private
+     */
+    async _getWalletTotalTokenBalanceBN(walletAddress, tokenContract) {
+
+        const walletUnstakedBalance = await tokenContract.methods.balanceOf(walletAddress).call();
+
+        return new BN(walletUnstakedBalance);
+
+    }
+
+    /**
+     * get the usd value of the wallet tokens balance
+     *
+     * @param web3
+     * @param walletAddress
+     * @param tokenAddress
+     * @param tokenContract
+     * @returns {Promise<BigNumber>}
+     * @private
+     */
+    async _getPositionWorthInUsdBN(web3, walletAddress, tokenAddress, tokenContract) {
+
+        const walletLpBalanceBN = await this._getWalletTotalTokenBalanceBN(
+            walletAddress,
+            tokenContract
+        );
+
+        if (walletLpBalanceBN.isGreaterThan(0)) {
+
+            const tokenDecimals = await tokenContract.methods.decimals().call()
+
+            const singleTokenWorthInUSD = await this.routerContract.methods.getAmountsOut(
+                (new BN("10").pow(tokenDecimals)), // single whole unit
+                [tokenAddress, WBNB_TOKEN_ADDRESS, USDC_TOKEN_ADDRESS]
+            ).call();
+
+            return new BN(singleTokenWorthInUSD[2]).div('1e' + this.usdcDecimals)
+                .multipliedBy(walletLpBalanceBN).dividedBy('1e' + tokenDecimals)
+
+        } else {
+
+            return new BN(0);
+
+        }
+
+    }
+
+
+}
+
+module.exports = TokensWorth;
