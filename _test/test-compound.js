268c28ae052b76f5893d4a9decad0fc4367862bf
Deddy Dayag <daviddayag@gmail.com>
2022-08-18 16:11:03 +0300

    compound notification fix

--- _test/test-compound.js
+++ _test/test-compound.js
@@ -1,32 +1,39 @@
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider(require('./api-keys.json').ethereum));
+const web3 = new Web3(new Web3.providers.HttpProvider(require('./dev-keys.json').web3));
 
-async function testVenusLowHealth() {
-  const LowHealth = require('../compound/low-liquidity');
-  const lowHealth = new LowHealth();
+async function testVenusLowHealth(address) {
+    const LowHealth = require('../compound/low-liquidity');
+    const lowHealth = new LowHealth();
 
-  // simulate init event
-  await lowHealth.onInit({
-    web3
-  });
+    // simulate init event
+    await lowHealth.onInit({
+        web3
+    });
 
-  // simulate on blocks event
-  let toBlock = await web3.eth.getBlockNumber();
-  return lowHealth.onBlocks({
-    web3,
-    toBlock: toBlock,
-    fromBlock: toBlock - 20,
-    subscription: {
-      minLiquidity: '600'
-    },
-    address: '0xde98e2e11cbb85486a844ed593fb0110f694d99a' // a random guy with balance
-  });
+    // simulate on blocks event
+    let toBlock = await web3.eth.getBlockNumber();
+    return lowHealth.onBlocks({
+        web3,
+        toBlock: toBlock,
+        fromBlock: toBlock - 20,
+        subscription: {
+            threshold: "0.5"
+        },
+        address: address
+    });
 }
 
 
 async function main() {
-  console.log('Running manual test:');
-  console.log(await testVenusLowHealth());
+
+    console.log('Running manual test:');
+
+    const address = '0x3dacC571356e7D5dFB3b475d6922442Ec06B9005';
+
+    console.log(await testVenusLowHealth(
+        address
+    ));
+
 }
 
 main();
