fa1dce9beb7494bf8d923ea773f87431773e9160
Degenddy <degenddy@users.noreply.github.com>
2022-08-18 16:11:03 +0300

    compound notification fix

--- compound/low-liquidity.js
+++ compound/low-liquidity.js
@@ -1,6 +1,9 @@
 const BigNumber = require("bignumber.js");
-const contracts = require('./contracts');
-const Contract = require('web3-eth-contract');
+const ABIs = require('./abis.json');
+
+
+const LENS_ADDRESS = '0xA6c8D1c55951e8AC44a0EaA959Be5Fd21cc07531'
+const COMPTROLLER_ADDRESS = '0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b'
 
 class LowLiquidity {
 
@@ -8,15 +11,32 @@ class LowLiquidity {
     static description = "Get notified when getting close to liquidation";
     static displayIcon = "hand";
 
-    // runs when class is initialized
+    /**
+     * runs when class is initialized
+     *
+     * @param args
+     * @returns {Promise<void>}
+     */
     async onInit(args) {
-        const abi = contracts.lens.abi
-        const addr = contracts.lens.addr
-        this.contract = _createContract(args.web3, addr, abi)
+
+        this.lensContract = new args.web3.eth.Contract(
+            ABIs.compoundLens,
+            LENS_ADDRESS
+        )
+
     }
 
+    /**
+     *
+     * @param args
+     * @returns {Promise<[{default: string, description: string, id: string, label: string, type: string}]>}
+     */
     async onSubscribeForm(args) {
-        const {markets, liquidity, shortfall} = await this.contract.methods.getAccountLimits(contracts.comptroller.addr, args.address).call();
+        const {
+            markets,
+            liquidity,
+            shortfall
+        } = await this.lensContract.methods.getAccountLimits(COMPTROLLER_ADDRESS, args.address).call();
 
         const roughExcess = new BigNumber(liquidity)
             .dividedBy(1e18) // full units
@@ -28,45 +48,56 @@ class LowLiquidity {
             .toString()
         const comment = markets.length > 0 && parseInt(shortfall) ?
             `CAUTION: you are currently in risk of liquidation` :
-            `Your current excess liquidity is ~${roughExcess} ETH`
+            `Your current excess liquidity is ~${roughExcess} USD`
         return [
             {
                 type: "input-number",
                 id: "minLiquidity",
                 label: "Minimum Liquidity",
                 default: defaultMin,
-                description: "The minimum desired liquidity denominated in ETH. " + comment
+                description: "The minimum desired liquidity denominated in USD. " + comment
             }
         ];
     }
 
-    // runs when new blocks are added to the mainnet chain - notification scanning happens here
+    /**
+     * runs when new blocks are added to the mainnet chain - notification scanning happens here
+     *
+     * @param args
+     * @returns {Promise<{notification: string}|*[]>}
+     */
     async onBlocks(args) {
-        const minBnB = new BigNumber(args.subscription['minLiquidity']).multipliedBy(1e18)
-        const {markets, liquidity, shortfall} = await this.contract.methods.getAccountLimits(contracts.comptroller.addr, args.address).call();
+
+        const threshold = new BigNumber(args.subscription['threshold']).multipliedBy(1e18)
+
+        const {
+            markets,
+            liquidity,
+            shortfall
+        } = await this.lensContract.methods.getAccountLimits(COMPTROLLER_ADDRESS, args.address).call();
 
         console.log(`markets: ${markets}, liquidity: ${liquidity}, shortfall: ${shortfall}`)
 
-        if (markets.length > 0 && minBnB.isGreaterThanOrEqualTo(new BigNumber(liquidity))) {
+        if (markets.length > 0 && threshold.isGreaterThanOrEqualTo(new BigNumber(liquidity))) {
+
             if (shortfall > 0) {
+
                 return {
                     notification: `Act now! You are under-collateralized and about to be liquidated`
                 }
+
             } else {
+
                 return {
                     notification: `Excess liquidity dropped below your safe minimum`
                 }
+
             }
+
         }
 
         return [];
     }
-}
-
-function _createContract(web3, address, abi) {
-    const contract = new Contract(abi, address)
-    contract.setProvider(web3.currentProvider)
-    return contract
 }
 
 module.exports = LowLiquidity;
