9911d7fb2834aec5adb6ca9d18155a526fd2353c
Deddy Dayag <daviddayag@gmail.com>
2022-11-02 14:26:51 +0200

    fixed twap network for polygon

--- twap/twap-all-polygon.js
+++ twap/twap-all-polygon.js
@@ -1,9 +1,11 @@
 const TwapAllBase = require("./twap-all-base");
 
 module.exports = class TwapAllFtm extends TwapAllBase {
+
     static displayName = "TWAP All Events";
     static description = "Get notified for all events regarding your TWAP orders (Polygon)";
-    static network = "fantom";
+
+    static network = "polygon";
 
     constructor() {
         super("0x8358686cf6dE08c89EE48016b6A40BBf1b1F9d3D");
--- twap/twap-order-completed-polygon.js
+++ twap/twap-order-completed-polygon.js
@@ -5,7 +5,7 @@ module.exports = class TwapOrderCompletedFtm extends TwapOrderCompletedBase {
     static displayName = "TWAP Order Completed";
     static description = "Get notified when your TWAP order is completely filled (Polygon)";
 
-    static network = "fantom";
+    static network = "polygon";
 
     constructor() {
         super("0x8358686cf6dE08c89EE48016b6A40BBf1b1F9d3D");
