8ccad6d24248fd3f3f17917a8d883a6844e18939
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-11-29 21:38:01 +0200

    fixed TWAP notifications filtering and refactor

---
+++ twap/twap-polygon-all.js
@@ -0,0 +1,11 @@
+const TwapBase = require("./twap-base");
+
+module.exports = class extends TwapBase {
+	static displayName = "TWAP All Events";
+	static description = "Get notified for all events regarding your TWAP orders (Polygon)";
+	static network = "polygon";
+
+	constructor() {
+		super("polygon");
+	}
+}
