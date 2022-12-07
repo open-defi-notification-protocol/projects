1e2a36b8ac8e21b0fbd43fefa9b33dce9218acc7
Degenddy <degenddy@users.noreply.github.com>
2022-12-07 17:21:48 +0200

    small fix to prevent base class from being read as a notification type

--- common/wallet-balance-polygon.js
+++ common/wallet-balance-polygon.js
@@ -2,7 +2,9 @@ const WalletBalanceBase = require("./wallet-balance-base");
 
 class WalletBalancePolygon extends WalletBalanceBase {
 
+    static displayName = "Wallet Balance (Polygon)";
     static network = "polygon"
+    static description = "Get notified when your wallet's balance in MATIC is above or below a certain threshold";
 
     constructor() {
 
