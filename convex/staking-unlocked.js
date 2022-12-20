42835a573d574c33b618a090438d7d98cee93bac
Degenddy <degenddy@users.noreply.github.com>
2022-12-20 15:04:20 +0200

    convex lock finished notification

--- convex/staking-unlocked.js
+++ convex/staking-unlocked.js
@@ -6,21 +6,21 @@ const amountFormatter = Intl.NumberFormat('en', {notation: 'compact'});
 class StakingUnlock {
 
     static displayName = "Staking Unlocked";
-    static description = "Get notified when your veCRV locked staking has finished";
+    static description = "Get notified when your vlCVX locked staking has finished";
 
     // runs when class is initialized
     async onInit(args) {
 
-        this.voteEscrowContract = new args.web3.eth.Contract(ABIs.voteEscrow, "0xC128a9954e6c874eA3d62ce62B468bA073093F25");
+        this.voteEscrowContract = new args.web3.eth.Contract(ABIs.voteEscrow, "0x72a19342e8F1838460eBFCCEf09F6585e32db86E");
 
-        this.veCRVDecimals = await this.voteEscrowContract.methods.decimals().call();
+        this.vlCVXDecimals = await this.voteEscrowContract.methods.decimals().call();
 
     }
 
     // runs right before user subscribes to new notifications and populates subscription form
     async onSubscribeForm(args) {
 
-        const balanceBN = new BN(await this.voteEscrowContract.methods.balanceOf(args.address).call());
+        const balanceBN = new BN(await this.voteEscrowContract.methods.lockedBalanceOf(args.address).call());
 
         const form = [];
 
@@ -42,16 +42,16 @@ class StakingUnlock {
 
     async onBlocks(args) {
 
-        let lockedParameters = await this.voteEscrowContract.methods.locked(args.address).call();
+        let lockedParameters = await this.voteEscrowContract.methods.lockedBalances(args.address).call();
 
-        const dateUnlocked = lockedParameters.end;
+        const dateUnlocked = lockedParameters.lockData[0].unlockTime;
 
         if (new Date().getTime() > (parseInt(dateUnlocked) * 1000)) {
 
-            const balanceBN = new BN(await this.voteEscrowContract.methods.balanceOf(args.address).call());
+            const balanceBN = new BN(await this.voteEscrowContract.methods.lockedBalanceOf(args.address).call());
 
             return [{
-                notification: `Your staking position of ${amountFormatter.format(balanceBN.dividedBy("1e" + this.veCRVDecimals))} veCRV is unlocked, go to curve.fi to claim it`
+                notification: `Your staking position of ${amountFormatter.format(balanceBN.dividedBy("1e" + this.vlCVXDecimals))} vlCVX is unlocked, go to convexfinance.com to claim it`
             }];
 
 
