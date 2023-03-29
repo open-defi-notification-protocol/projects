2b17129310e7ea6d43dadeb883d328ea20819297
Degenddy <degenddy@users.noreply.github.com>
2023-03-29 10:33:14 +0300

    small fix to y2k

--- y2k/market-created.js
+++ y2k/market-created.js
@@ -1,6 +1,9 @@
 const ABIs = require("./abis.json");
+const BN = require("bignumber.js");
 
 const VAULT_FACTORY = "0x984E0EB8fB687aFa53fc8B33E12E04967560E092";
+
+const amountFormatter = Intl.NumberFormat('en');
 
 class MarketCreated {
 
@@ -70,12 +73,15 @@ class MarketCreated {
 
                 const mIndex = returnValues.mIndex;
 
+                const {decimals, symbol} = await this._fetchTokenDetails(args.web3, returnValues.token);
+
+                const strikePrice = new BN(returnValues.strikePrice).dividedBy('1e' + decimals);
 
                 const uniqueId = "mIndex-event-" + mIndex;
 
                 notifications.push({
                     uniqueId: uniqueId,
-                    notification: `New Market created [${returnValues.name}]! Go to https://y2k.finance to participate.`
+                    notification: `New Market created - ${symbol} (strike price: ${amountFormatter.format(strikePrice)})! Go to https://y2k.finance to participate.`
                 });
 
             }
@@ -86,6 +92,23 @@ class MarketCreated {
 
     }
 
+    /**
+     *
+     * @param web3
+     * @param address
+     * @returns {Promise<{symbol: *, decimals: *}>}
+     * @private
+     */
+    async _fetchTokenDetails(web3, address) {
+
+        const tokenContract = new web3.eth.Contract(ABIs.erc20, address);
+
+        const decimals = await tokenContract.methods.decimals().call();
+        const symbol = await tokenContract.methods.symbol().call();
+
+        return {decimals, symbol}
+
+    }
 
 }
 
