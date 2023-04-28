29a9940749c3ef4ca9110d9b230ce47f6e237204
pe-matsushita <106372336+pe-matsushita@users.noreply.github.com>
2023-04-28 17:13:11 +0900

    contract address has been updated and logo improved.

--- _test/test-lootbridge.js
+++ _test/test-lootbridge.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(require('./dev-keys.json').web3));
 
 async function testMintyardLowHealth(address, threshold) {
-    const LowHealth = require('../mintyard/low-health');
+    const LowHealth = require('../lootbridge/low-health');
     const lowHealth = new LowHealth();
 
     // simulate init event
