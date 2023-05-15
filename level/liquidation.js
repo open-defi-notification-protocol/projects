2057c22985404e8e2a58f19391a9d2ea0a00c769
Deddy Dayag <daviddayag@gmail.com>
2023-05-15 16:22:35 +0300

    level wip

--- level/liquidation.js
+++ level/liquidation.js
@@ -25,6 +25,10 @@ class Liquidation {
             POOL_CONTRACT_ADDRESS
         );
 
+        const {indexTokens, stableTokens} = await this._getTokensList(args.web3);
+        this.indexTokens = indexTokens;
+        this.stableTokens = stableTokens;
+
         this.orderManagerContract = new args.web3.eth.Contract(
             ABIs.orderManager,
             ORDER_MANAGER_CONTRACT_ADDRESS
@@ -61,9 +65,7 @@ class Liquidation {
      */
     async onBlocks(args) {
 
-        const pairAddress = args.subscription["pair"];
-
-        const userLTVBN = await this._getUserLTVBN(args, pairAddress);
+        const positionKey = args.subscription["position"];
 
         const threshold = args.subscription["threshold"];
 
@@ -72,7 +74,7 @@ class Liquidation {
             const pairLabel = await this._getPairLabel(args, pairAddress, userLTVBN);
 
             return {
-                uniqueId: pairAddress + "-" + threshold,
+                uniqueId: positionKey + "-" + threshold,
                 notification: `The LTV of your position in ${pairLabel} is above the threshold of ${args.subscription["threshold"]}`
             };
 
@@ -89,12 +91,43 @@ class Liquidation {
      */
     async _getAllUserPositions(args) {
 
-        const orderIds = (await this.orderManagerContract.methods.getOrders(
-            args.address,
-            0,
-            100
-        ).call())[0]
+        /*
+                const orderIds = (await this.orderManagerContract.methods.getOrders(
+                    args.address,
+                    0,
+                    100
+                ).call())[0]
+        */
 
+        const positionKeys = {}
+
+        for (const indexToken of this.indexTokens) {
+
+            const longPosKey = args.web3.utils.keccak256(args.web3.eth.abi.encodeParameters(
+                ["address", "address", "address", "uint8"],
+                [args.address, indexToken, indexToken, 0]));
+
+            positionKeys[longPosKey] = {
+                indexToken,
+                collateralToken: indexToken,
+                side: 0
+            };
+
+            for (const stableToken of this.stableTokens) {
+
+                let shortPosKey = args.web3.utils.keccak256(args.web3.eth.abi.encodeParameters(
+                    ["address", "address", "address", "uint8"],
+                    [args.address, indexToken, stableToken, 1]));
+
+                positionKeys[shortPosKey] = {
+                    indexToken,
+                    collateralToken: stableToken,
+                    side: 1
+                };
+
+            }
+
+        }
 
         const positions = [];
 
@@ -102,18 +135,16 @@ class Liquidation {
 
         const contractCallContext = [];
 
-        const pools = await this.masterchefContract.methods.poolLength().call();
-
         // starting from 1 bcs pool 0 is the cake token and will be called specifically later.
-        for (let poolId = 1; poolId < pools; poolId++) {
+        for (const positionKey in positionKeys) {
 
             contractCallContext.push({
-                reference: 'masterchef-poolId-' + poolId,
-                contractAddress: MASTERCHEF_ADDRESS,
-                abi: ABIs.masterchef,
-                calls: [{reference: 'userInfoCall', methodName: 'userInfo', methodParameters: [poolId, args.address]}],
+                reference: 'pool-positionKey-' + positionKey,
+                contractAddress: POOL_CONTRACT_ADDRESS,
+                abi: ABIs.pool,
+                calls: [{reference: 'positionsCall', methodName: 'positions', methodParameters: [positionKey]}],
                 context: {
-                    poolId: poolId
+                    positionKey: positionKey
                 }
             });
 
@@ -123,16 +154,20 @@ class Liquidation {
 
         for (const result of Object.values(results)) {
 
-            const userStakedBalanceBN = new BN(result.callsReturnContext[0].returnValues[0].hex);
+            const positionValues = result.callsReturnContext[0].returnValues;
 
-            if (userStakedBalanceBN.isGreaterThan("0")) {
+            if (new BN(positionValues[1].hex).isGreaterThan("0")) {
 
-                const poolId = result.originalContractCallContext.context.poolId;
+                const positionKey = result.originalContractCallContext.context.positionKey;
 
-                const poolLabel = await this._getPairLabel(args, poolId);
+                const poolLabel = await this._getPairLabel(
+                    args,
+                    positionKeys[positionKey],
+                    positionValues
+                );
 
                 positions.push({
-                    value: poolId,
+                    value: positionKey,
                     label: poolLabel
                 });
 
@@ -144,51 +179,46 @@ class Liquidation {
 
     }
 
-    /**
-     *
-     * @param args
-     * @param pairAddress
-     * @returns {Promise<BigNumber>}
-     * @private
-     */
-    async _getUserLTVBN(args, pairAddress) {
+    async _getTokensList(web3) {
 
-        const fraxlendPairHelperContract = new args.web3.eth.Contract(ABIs.pairHelperAbi, FRAXLEND_PAIR_HELPER_ADDRESS);
+        const assetCount = parseInt(await web3.eth.getStorageAt(POOL_CONTRACT_ADDRESS, '0xa2')); // read length of assets array
 
-        const collateral_1usd_rate = await fraxlendPairHelperContract.methods.previewUpdateExchangeRate(pairAddress).call();
+        const indexTokens = [];
+        const stableTokens = [];
 
-        // Get some user accounting information
+        for (let i = 0; i < assetCount; i++) {
 
-        let result = await fraxlendPairHelperContract.methods.getUserSnapshot(pairAddress, args.address).call();
+            const token = await this.poolContract.methods.allAssets(i).call();
 
-        const borrowSharesBN = new BN(result[1])
-        const collateralBalance = result[2]
+            // now you may want to check if this token is stable coin
+            const isStable = await this.poolContract.methods.isStableCoin(token).call();
 
-        result = await fraxlendPairHelperContract.methods.getPairAccounting(pairAddress).call();
-        const totalBorrowAmount = result[2]
-        const totalBorrowShares = result[3]
+            if (isStable) {
+                stableTokens.push(token);
+            } else {
+                indexTokens.push(token);
+            }
+        }
 
-        // Calculate LTV
-
-        const userBorrowAmountBN = borrowSharesBN.multipliedBy(totalBorrowAmount).dividedBy(totalBorrowShares);
-
-        // taken from the frax liquidator bot code (https://github.com/FraxFinance/fraxlend/blob/2541696d98441afafed7a380e0b7a46c638a5c00/src/liquidatorBot/bot.mjs)
-        return userBorrowAmountBN.multipliedBy(collateral_1usd_rate).dividedBy('1e18').multipliedBy('1e5').dividedBy(collateralBalance).dividedBy('1e3');
+        return {indexTokens, stableTokens};
 
     }
 
     /**
      *
      */
-    async _getPairLabel(args, pairAddress, userLTV) {
+    async _getPairLabel(args, positionInfo, positionValues) {
 
-        const pairContract = new args.web3.eth.Contract(ABIs.pair, pairAddress);
+        const indexTokenContract = new args.web3.eth.Contract(ABIs.erc20, positionInfo.indexToken);
+        const collateralTokenContract = new args.web3.eth.Contract(ABIs.erc20, positionInfo.collateralToken);
 
-        let pairLabel = await pairContract.methods.symbol().call();
+        const indexTokenLabel = await indexTokenContract.methods.symbol().call();
+        const collateralTokenLabel = await collateralTokenContract.methods.symbol().call();
 
-        pairLabel = pairLabel.replace('FraxlendV1 - ', '')
+        const sizeValue = amountFormatter.format(new BN(positionValues[0].hex).dividedBy('1e30').toFixed());
+        const collateralValue = amountFormatter.format(new BN(positionValues[1].hex).dividedBy('1e30').toFixed());
 
-        return `${pairLabel} (${amountFormatter.format(userLTV)})`;
+        return `${positionInfo.side === 1 ? 'Short' : 'Long'} position, Size: ${sizeValue} ${indexTokenLabel}, Collateral: ${collateralValue} ${collateralTokenLabel}`;
 
     }
 }
