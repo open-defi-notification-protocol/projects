a353b24eae2c003dc4702593055c657a6b9ed428
Deddy Dayag <daviddayag@gmail.com>
2023-05-17 11:20:12 +0300

    level liquidation notification

--- _test/test-level.js
+++ _test/test-level.js
@@ -5,7 +5,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(require('./dev-keys.json')
  * testing pending rewards notification
  *
  */
-async function testLiquidation(address) {
+async function testLiquidation(address, closedPositionKey) {
 
     const Liquidation = require('../level/liquidation');
     const liquidation = new Liquidation();
@@ -26,9 +26,14 @@ async function testLiquidation(address) {
     // simulate on blocks event
     return liquidation.onBlocks({
         web3,
-        fromBlock: 74015797,
-        toBlock: 76540287
+        address,
+        subscription: {
+            position: closedPositionKey
+        },
+        fromBlock: 28280169,
+        toBlock: 28284169
     });
+
 }
 
 
@@ -38,10 +43,10 @@ async function main() {
 
     console.log(
         await testLiquidation(
-            '0x3dacC571356e7D5dFB3b475d6922442Ec06B9005'
+            '0x3dacC571356e7D5dFB3b475d6922442Ec06B9005',
+            '0xb0bfca8fbee0bcc76bce0010264b9826bbfefd1a3684df653c5a5bcb9536546f'
         )
     );
-
 
 }
 
