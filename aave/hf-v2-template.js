3650baf26e52c3015f61679dfbbe90f51631fa37
defispartan <defispartan@gmail.com>
2024-04-28 23:48:19 -0400

    feat: aave templates

---
+++ aave/hf-v2-template.js
@@ -0,0 +1,70 @@
+const ABIs = require("./abis.json");
+const addresses = require("./addresses.json");
+const BN = require("bignumber.js");
+
+const amountFormatter = Intl.NumberFormat("en");
+
+class V2PoolBase {
+  static displayName = "Position Health";
+  static description = "Get notified when Health Factor is getting below a certain threshold";
+
+  /**
+   *
+   * @param network
+   */
+  constructor(network) {
+    this.network = network;
+    this.poolContractAddress = addresses["V2"][network];
+  }
+
+  /**
+   *
+   * @param args
+   * @returns {Promise<void>}
+   */
+  async onInit(args) {
+    this.contract = new args.web3.eth.Contract(ABIs.lendingPool, this.poolContractAddress);
+  }
+
+  async onSubscribeForm(args) {
+    const healthFactorBN = await this._getPositionHealth(args);
+
+    if (healthFactorBN !== "no-position") {
+      return [
+        {
+          type: "input-number",
+          id: "threshold",
+          label: `Health Factor Threshold (current is ${amountFormatter.format(healthFactorBN.toString())})`,
+          default: "1.1",
+          description: "Notify me when the Health Factor of my position goes below this threshold.",
+        },
+      ];
+    } else {
+      return [];
+    }
+  }
+
+  async onBlocks(args) {
+    const threshold = args.subscription["threshold"];
+
+    const uniqueId = "aave-health-" + this.network + "-v2-" + threshold;
+
+    const healthFactorBN = await this._getPositionHealth(args);
+
+    if (healthFactorBN !== "no-position" && healthFactorBN.isLessThan(threshold))
+      return {
+        uniqueId: uniqueId,
+        notification: `Current Health Factor (${amountFormatter.format(healthFactorBN.toString())}) is below the threshold of ${threshold}!`,
+      };
+
+    return [];
+  }
+
+  async _getPositionHealth(args) {
+    const position = await this.contract.methods.getUserAccountData(args.address).call();
+
+    return new BN(position.totalDebtETH).isGreaterThan(0) ? new BN(position.healthFactor).dividedBy("1e18") : "no-position";
+  }
+}
+
+module.exports = V2PoolBase;
